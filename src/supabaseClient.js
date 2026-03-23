5c104aef054e5559b00ed328008ec8b29722e1ce
Mariam Hosini <hossinimariam93@example.com>
2026-03-23 23:37:05 +0200

    update supabase key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://cykvxpbamzcqnieokpzl.supabase.co' 
-const supabaseAnonKey = "sb_publishable_G4kr77vrzZnuzeK4-NEhAw_-z01fwBl"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5a3Z4cGJhbXpjcW5pZW9rcHpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NzA0ODgsImV4cCI6MjA4ODA0NjQ4OH0.V9fIbfZWRhaAGJuKsuV0Pbdk_vfaUDQ4Wgm1mqxaqxo"
 const supabase = createClient(supabaseUrl ,supabaseAnonKey );
 export default supabase;
