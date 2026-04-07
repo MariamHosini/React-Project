940ec2e18bd53e2164f1aeee1df096612619777f
Mariam Hosini <hossinimariam93@example.com>
2026-04-07 23:00:15 +0200

    profile

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import modelImage from "../../assets/model2.png";
 import backGround from "../../assets/bg.webp";
-import { NavLink } from "react-router-dom";
+
 export default function Home() {
   const navigate = useNavigate();
   return (
